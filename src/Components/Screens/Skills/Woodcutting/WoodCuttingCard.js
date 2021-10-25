d6e6fcfd48a2a7902118d8271225a3ae637fd48d
Zachary Monk <ZMonk91@gmail.com>
2021-10-25 08:37:09 -0700

    Final Commit before switching to Chakra-ui

--- src/Components/Screens/Skills/Woodcutting/WoodCuttingCard.js
+++ src/Components/Screens/Skills/Woodcutting/WoodCuttingCard.js
@@ -4,20 +4,21 @@ import { Button, Card, CardText, CardTitle } from "reactstrap";
 
 export function WoodcuttingCard(props){
     return (
-			<Card className="WoodcuttingCard">
+			<Card className="ActionCard">
 				<img
 					className="ActionCard--image"
 					width="100%"
 					src={require('./images/grassland_bg.jpeg').default}
 					alt=""
 				/>
-				<CardTitle tag="h5" className="ActionCard--Title">
+				<CardTitle tag="h5" className="ActionCard--title">
 					title
 				</CardTitle>
 				<CardText className="ActionCard--text">
 					1 2.33 5 xp/hr 10 minutes
 				</CardText>
 				<Button className="ActionCard--button">Search</Button>
+
 			</Card>
 		)
 }
