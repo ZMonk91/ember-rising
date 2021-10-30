7ace0c4a017d75ab08b417f6d135e82b0f1f8cb1
Zachary Monk <ZMonk91@gmail.com>
2021-10-30 09:42:51 -0700

    removed bootstrap dependency for build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import "bootstrap/dist/css/bootstrap.min.css"
 import App from "./App"
 import store from "./app/store"
 import {Provider} from "react-redux"
